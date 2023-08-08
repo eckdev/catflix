b9f94afa9360cb447bfcc0758190ca401883e08a
Eyüp Can Kayadarçin <eck@Eyup-MacBook-Pro.local>
2023-08-08 14:03:24 +0300

    adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1338120620743783"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
